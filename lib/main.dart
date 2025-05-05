f0cbc08d24cb5ee6858f42c50b4d4c415179165f
MuhammadFajarAriansyah <muhammadfajarariansyah_2327250082@mhs.mdp.ac.id>
2025-05-05 09:30:22 +0700

    Splash Screen dan Icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
-import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fasum/firebase_options.dart';
-import 'package:fasum/screens/home_screen.dart';
-import 'package:fasum/screens/sign_in_screen.dart';
+import 'package:fasum/screens/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,19 +20,10 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Fasum',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
-      home: StreamBuilder(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return const HomeScreen();
-          } else {
-            return const SignInScreen();
-          }
-        },
-      ),
+      home: SplashScreen(),
     );
   }
 }
